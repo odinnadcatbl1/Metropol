89c686ad842b747418a72d457f010802a43e98ba
Salavat F <salmrproperfahr@mail.ru>
2021-11-16 10:26:54 +0300

    fixed promotions, add yandex-map script, add jquery-match-height

--- images/js/main.js
+++ images/js/main.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
 
-    let catalogSlider = $(".catalog__slider");
+    if($('.js-svg-img').length){
+		$('.js-svg-img').each(function(){
+			
+			var $img = $(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+			
+			console.log(imgURL);
+			
+			$.get(imgURL, function(data) {
+				var $svg = $(data).find('svg');
+				
+				console.log($svg);
+				
+				
+				if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+				$svg = $svg.removeAttr('xmlns:a');
+				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+				}
+				$img.replaceWith($svg);
+				$svg.addClass('ready');
+
+			}, 'xml');
+		});
+	}
+
+
+    let catalogSlider = $(".js-catalog__slider");
 
     catalogSlider.slick({
         infinite: true, // если элементы заканчиваются - они повторяются заново
@@ -10,7 +44,7 @@ $(document).ready(function(){
         arrows: true,
     });
 
-    let reviewsSlider = $(".reviews__slider");
+    let reviewsSlider = $(".js-reviews__slider");
 
     reviewsSlider.slick({
         infinite: true, // если элементы заканчиваются - они повторяются заново
@@ -22,5 +56,6 @@ $(document).ready(function(){
     });
 
 
+    $('.js-title-height').matchHeight();
 
 });
